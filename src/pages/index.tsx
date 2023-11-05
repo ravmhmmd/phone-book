f8d2b9a202944ac527a64d398e38ff81ac15bcb5
Muhammad Rayhan Ravianda <rayhanrrav@gmail.com>
2023-11-05 16:59:26 +0700

    add metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,39 @@ export default function Home() {
 				<title>YellowPage</title>
 				<meta name="description" content="Online people's contact dictionary" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					property="og:url"
+					content="https://yellowpage-phonebook.netlify.app/"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="YellowPage" />
+				<meta
+					property="og:description"
+					content="Online people's contact dictionary"
+				/>
+				<meta
+					property="og:image"
+					content="https://yellowpage-phonebook.netlify.app/apple-touch-icon.png"
+				/>
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:domain"
+					content="yellowpage-phonebook.netlify.app"
+				/>
+				<meta
+					property="twitter:url"
+					content="https://yellowpage-phonebook.netlify.app/"
+				/>
+				<meta name="twitter:title" content="YellowPage" />
+				<meta
+					name="twitter:description"
+					content="Online people's contact dictionary"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://yellowpage-phonebook.netlify.app/apple-touch-icon.png"
+				/>
 			</Head>
 			<main>
 				<Phonebook />
